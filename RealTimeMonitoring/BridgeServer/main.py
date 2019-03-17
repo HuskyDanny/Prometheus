0d12e6a42fdb4bfec51face6cfabf2a125bcfa15
Junchen Pan <junchenp1018@gmail.com>
2019-03-17 16:52:32 -0700

    Mount uwsgi app to flask

--- RealTimeMonitoring/BridgeServer/main.py
+++ RealTimeMonitoring/BridgeServer/main.py
@@ -1,7 +1,8 @@
 from flask import Flask, jsonify, render_template, request
-from prometheus_client import Gauge, start_http_server
-from adapter import write, monitor
-from output import outToCsv
+from prometheus_client import Gauge, start_http_server, make_wsgi_app
+from werkzeug.wsgi import DispatcherMiddleware
+from adapter import adapter
+from output import output
 from wtforms import Form, TextAreaField, validators
 
 
@@ -24,11 +25,31 @@ def toCSV():
     form = ReviewForm(request.form)
     return render_template('index.html', form = form)
 
+@app.route('/write', methods=['POST', 'GET'])
+def addMetrics():
+    if request.method == 'POST':
+        data = request.get_json()
+        adapter.write(data)
+        return 'Finished addMetrics'
+    else:
+        return "Accessing metrics..." + adapter.showMetrics().decode("utf-8")
 
+@app.route('/todb', methods=['POST'])
+def todb():
+    if request.method == 'POST':
+        data = request.form.to_dict()
+        
+        metricname = request.form['metricname']
+        filename = request.form['filename']
+        period = request.form['period']
+        # #data = request.get_json()
+        output.outToCsv(filename, metricname, prometheus_host, period)
+        return "Finished todb"
 
-#start http server for data scraping
-monitor(8000)
+app.wsgi_app = DispatcherMiddleware(app.wsgi_app, {
+    '/metrics': make_wsgi_app()
+})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port = 80)
     
